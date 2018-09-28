be31965b030a1a2b9b598ab4c56bf8df012492af
ironside2001 <ironside2001@naver.com>
2018-09-29 07:00:18 +0900

    [Add] 초기 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">todo_practice</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
