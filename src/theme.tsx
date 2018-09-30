a75190ca8bc50199c2f815f6f3c66a1ddb059408
Kyungwook Cho <amisay5256@naver.com>
2018-09-30 23:52:58 +0900

    [Add] theme 파일 추가

---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+        main: '#6200EE'
+    },
+    secondary: {
+        main: '#03DAC5'
+    }
+  },
+});
+
+export default theme
