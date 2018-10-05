5c98ddb9eac05e037b8bcab29bfb8e5fedea70dd
Kyungwook Cho <amisay5256@naver.com>
2018-10-05 13:08:57 +0900

    [Add] 로그인 기능 추가

--- src/LoginCard.tsx
+++ src/LoginCard.tsx
@@ -4,20 +4,22 @@ import Card from '@material-ui/core/Card';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import TextField from '@material-ui/core/TextField';
 import axios, { AxiosError, AxiosResponse } from 'axios';
+import { action, observable } from 'mobx';
+import { observer } from 'mobx-react';
 import * as React from 'react';
 import { korTheme } from './theme';
 
 const styles = createStyles({
     root: {
-
+        display: 'flex'
     },
     card: {
+        minWidth: '344px',
+        minHeight: '220px',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center',
-        minWidth: '344px',
-        minHeight: '218px'
+        alignItems: 'center'
     },
     idTextField: {
         marginBottom: '18px'
@@ -32,31 +34,43 @@ const styles = createStyles({
 
 type ClassNames = WithStyles<keyof typeof styles>;
 
-const baseUrl: string = 'https://practice.alpaca.kr/api'
-
-const login = () => {
-    axios.post(baseUrl + '/users/login/', {
-        username: "user1",
-        password: "qwerrewq"
-    })
-    .then((response: AxiosResponse) => {
-        console.log(response);
-    })
-    .catch((err: AxiosError) => {
-        console.log(err);
-    });
-}
-
+@observer
 class LoginCard extends React.Component<ClassNames> {
+    @observable private username = '';
+    @observable private password = '';
+
+    @action
+    private handleChangeUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.username = event.currentTarget.value;
+    }
+
+    @action
+    private handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.password = event.currentTarget.value;
+    }
+
+    private login = () => {
+        axios.post('https://practice.alpaca.kr/api/users/login/', {
+            username: this.username,
+            password: this.password
+        })
+        .then((response: AxiosResponse) => {
+            console.log(response);
+        })
+        .catch((err: AxiosError) => {
+            console.log(err);
+        });
+    }
+
     public render() {
         const { classes } = this.props;
         return (
             <div className={classes.root}>
                 <MuiThemeProvider theme={korTheme}>
                     <Card className={classes.card}>
-                        <TextField className={classes.idTextField} label="아이디" />
-                        <TextField className={classes.pwTextField} label="비밀번호" type="password" />
-                        <Button className={classes.loginButton} variant="contained" color="primary" onClick={login}>로그인</Button>
+                        <TextField className={classes.idTextField} label="아이디" value={this.username} onChange={this.handleChangeUsername} />
+                        <TextField className={classes.pwTextField} label="비밀번호" type="password" value={this.password} onChange={this.handleChangePassword} />
+                        <Button className={classes.loginButton} variant="contained" color="primary" onClick={this.login}>로그인</Button>
                     </Card>
                 </MuiThemeProvider>
             </div>
