9b5a4a397a553495361fd292607beef5c85fd632
Kyungwook Cho <amisay5256@naver.com>
2018-10-06 09:56:53 +0900

    [Add] MyAppBar 컴포넌트에 로그아웃 버튼 추가

--- src/MyAppBar.tsx
+++ src/MyAppBar.tsx
@@ -1,30 +1,46 @@
 import AppBar from '@material-ui/core/AppBar';
-import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import { createStyles, withStyles } from '@material-ui/core/styles';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import { inject, observer } from 'mobx-react';
 import * as React from 'react';
-import { engTheme } from './theme';
+import AppStore from './stores/app'
+import { engTheme, korTheme } from './theme';
 
 const styles = createStyles({
     root: {
 
+    },
+    title: {
+        flexGrow: 1
+    },
+    buttonLogout: {
+        color: 'white',
+        fontSize: '16px'
     }
 });
 
-type ClassNames = WithStyles<keyof typeof styles>;
-
-class MyAppBar extends React.Component<ClassNames> {
+@inject('app')
+@observer
+class MyAppBar extends React.Component {
     public render() {
-        const { classes } = this.props;
+        const app = this.props as AppStore;
         return (
-            <div className={classes.root}>
+            <div className={styles.root.toString()}>
                 <MuiThemeProvider theme={engTheme}>
                     <AppBar color='primary'>
                         <Toolbar>
-                            <Typography variant="title" color="inherit">
+                            <Typography className={styles.title.toString()} variant="title" color="inherit">
                                 TODO Practice
                             </Typography>
+
+                            {app.isLogined ? (
+                            <MuiThemeProvider theme={korTheme}>
+                                <Button className={styles.buttonLogout.toString()}>로그아웃</Button>
+                            </MuiThemeProvider>
+                            ) : ('')}
                         </Toolbar>
                     </AppBar>
                 </MuiThemeProvider>
